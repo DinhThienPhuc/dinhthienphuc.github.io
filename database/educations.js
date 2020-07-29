8a7a34ea91e02c0cd6c85dc21be62036f69a463a
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-07-29 16:21:31 +0700

    Mod: update user interface

---
+++ database/educations.js
@@ -0,0 +1,8 @@
+const educations = [
+  {
+    school: "Hanoi University of Science and Technology",
+    studyField: "Engineering Physics",
+    start: "2013",
+    end: "2018",
+  },
+];
---
+++ database/skills.js
@@ -0,0 +1,26 @@
+const skills = [
+  {
+    group: "Programming Languages",
+    skills: [],
+  },
+  {
+    group: "Frameworks & Platforms",
+    skills: [],
+  },
+  {
+    group: "Database",
+    skills: [],
+  },
+  {
+    group: "Version Control",
+    skills: [],
+  },
+  {
+    group: "Foreign Languages",
+    skills: [],
+  },
+  {
+    group: "Knowledge",
+    skills: [],
+  },
+];
